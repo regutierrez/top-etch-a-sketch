c2f9202b55452014b9ff46e5787d713307039b9f
regutierrez <11924623+regutierrez@users.noreply.github.com>
2022-12-16 22:12:54 +0800

    feat: - add refreshBoard function - centered color picker button and size text

--- index.js
+++ index.js
@@ -3,40 +3,45 @@ let mouseDown = false;
 document.body.addEventListener('mousedown', () => (mouseDown = true));
 document.body.addEventListener('mouseup', () => (mouseDown = false));
 
-//Slider functions
+const container = document.querySelector('#container');
 const slider = document.getElementById('slider');
 const size = document.getElementById('size');
+
+//Slider function
 slider.addEventListener('change', (e) => {
     changeBoardSize(e.target.value);
 });
 
-
 function changeBoardSize(gridSize) {
     updateSize(gridSize);
-    createDrawingBoard(gridSize);
+    refreshBoard();
+    createBoard(gridSize);
      console.log(gridSize);  
+}
+
+function refreshBoard() {
+    container.innerHTML = ''
 }
 
 function updateSize(newValue) {
     size.innerHTML = `${newValue} x ${newValue}`;
 }
 
-function createDrawingBoard(gridSize) {
-    const container = document.querySelector('#container');
+function createBoard(gridSize) {
     container.style.setProperty('--grid-num', gridSize); 
     for(let i = 0; i < (gridSize**2) ; i++) {
         const cell = document.createElement('div');
-        cell.addEventListener('mouseover', changeColor);
-        cell.addEventListener('mousedown', changeColor);
+        cell.addEventListener('mouseover', draw);
+        cell.addEventListener('mousedown', draw);
         container.appendChild(cell).className = "cell";
     }; 
 }
 
-function changeColor(e) {
+function draw(e) {
     // if not clicked AND hovered, do not change color
     if (e.type === 'mouseover' && !mouseDown) return;
     e.target.style.background = 'black';
 }
 
-createDrawingBoard(slider.value);
+createBoard(slider.value);
 
