a8816b7811b4ca066a7bc167ff38ff074c21dc84
regutierrez <11924623+regutierrez@users.noreply.github.com>
2022-12-17 13:41:18 +0800

    feat: add rainbow drawing functionality

--- index.js
+++ index.js
@@ -13,52 +13,50 @@ const shade = document.getElementById('shade');
 const eraser = document.getElementById('eraser');
 const clear = document.getElementById('clear');
 
+let currentMode = 'color'; 
+let gridSize = slider.value;
+
 //Change modes
 //click btn => change mode (btn becomes active, others are deactivated)
 // => btn gets highlighted => 
-colorBtn.addEventListener('click', function() {
-    changeMode(colorBtn.id);
-});
-rainbowBtn.addEventListener('click', function() {
-    changeMode(rainbowBtn.id);
-});
-shade.addEventListener('click', function(){
-    changeMode(shade.id);
-});
-eraser.addEventListener('click', function(){
-    changeMode(eraser.id);
-});
-clear.addEventListener('click', function() {
-    refreshBoard();
-});
+colorBtn.addEventListener('click', () => changeMode(colorBtn.id));
+rainbowBtn.addEventListener('click', () => changeMode(rainbowBtn.id));
+shade.addEventListener('click', () => changeMode(shade.id));
+eraser.addEventListener('click', () => changeMode(eraser.id));
+clear.addEventListener('click', () => refreshBoard(gridSize));
 
 function changeMode(mode) {
+    currentMode = mode;
+    activateBtn(currentMode);    
+}
 
+function activateBtn(mode) {
+    console.log(mode);
 }
 
 //Slider function
 slider.addEventListener('change', (e) => {
-    changeBoardSize(e.target.value);
+    gridSize = e.target.value;
+    changeBoardSize(gridSize);
 });
 
-function changeBoardSize(gridSize) {
-    updateSize(gridSize);
-    refreshBoard();
-    createBoard(gridSize);
-     console.log(gridSize);  
+function changeBoardSize(inputSize) {
+    updateSize(inputSize);
+    refreshBoard(inputSize);
 }
 
-function refreshBoard() {
-    container.innerHTML = ''
+function refreshBoard(inputSize) {
+    container.innerHTML = '';
+    createBoard(inputSize);
 }
 
 function updateSize(newValue) {
     size.innerHTML = `${newValue} x ${newValue}`;
 }
 
-function createBoard(gridSize) {
-    container.style.setProperty('--grid-num', gridSize); 
-    for(let i = 0; i < (gridSize**2) ; i++) {
+function createBoard(inputSize) {
+    container.style.setProperty('--grid-num', inputSize); 
+    for(let i = 0; i < (inputSize**2) ; i++) {
         const cell = document.createElement('div');
         cell.addEventListener('mouseover', draw);
         cell.addEventListener('mousedown', draw);
@@ -66,10 +64,26 @@ function createBoard(gridSize) {
     }; 
 }
 
+const randomRGB = () => Math.floor(Math.random() * 256);
+
 function draw(e) {
     // if not clicked AND hovered, do not change color
     if (e.type === 'mouseover' && !mouseDown) return;
-    e.target.style.background = 'black';
+    if (currentMode == colorBtn.id) {  
+        e.target.style.background = 'black';
+    }
+    if (currentMode == rainbowBtn.id) {
+        const randomR = randomRGB();
+        const randomG = randomRGB();
+        const randomB = randomRGB();
+        e.target.style.background = `rgb(${randomR},${randomG},${randomB})`;
+    }
+    if (currentMode === shade.id) {
+        
+    }
+    if (currentMode === eraser.id) {
+        
+    }
 }
 
 createBoard(slider.value);
